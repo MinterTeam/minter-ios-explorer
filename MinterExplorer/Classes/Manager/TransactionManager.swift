09f7458c5cfb0d3e259901606341778bdd75d2b3
Alexey Sidorov <ody344@gmail.com>
2018-05-19 21:10:09 +0300

    Added first manager

---
+++ MinterExplorer/Classes/Manager/TransactionManager.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionManager.swift
+//  Alamofire
+//
+//  Created by Alexey Sidorov on 19/05/2018.
+//
+
+import Foundation
+import MinterCore
+
+
+class TransactionManager {
+	
+	
+	
+}
+
